5212aa1bdb4cc50c3cd72540fc4bc487a0e79643
Huiming Teo <teohuiming@gmail.com>
2017-06-23 10:29:10 +0800

    Add oh-my-zsh customization files

---
+++ zsh_custom/aliases.zsh
@@ -0,0 +1,6 @@
+alias g=git
+alias els=elasticsearch
+alias redis="redis-server /usr/local/etc/redis.conf"
+alias pg="postgres -D /usr/local/var/postgres"
+alias streamflix="~/streamflix/bin/streamflix"
+alias dots='git --git-dir=$HOME/dots/ --work-tree=$HOME'
---
+++ zsh_custom/exports.zsh
@@ -0,0 +1,3 @@
+export EDITOR=vim
+export LANG=en_US.UTF-8
+export PATH="~/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
