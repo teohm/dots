f80841d8f83c6b2efe4f3f37a5ead9e07d953e84
Huiming Teo <teohuiming@gmail.com>
2017-06-23 11:16:39 +0800

    Rename dots bare git repo to .dots

--- zsh_custom/aliases.zsh
+++ zsh_custom/aliases.zsh
@@ -3,4 +3,4 @@ alias els=elasticsearch
 alias redis="redis-server /usr/local/etc/redis.conf"
 alias pg="postgres -D /usr/local/var/postgres"
 alias streamflix="~/streamflix/bin/streamflix"
-alias dots='git --git-dir=$HOME/dots/ --work-tree=$HOME'
+alias dots='git --git-dir=$HOME/.dots/ --work-tree=$HOME'
