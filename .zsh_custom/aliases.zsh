10e2a3995b16cdc8ce7ff7d3e1912a4054a67020
Huiming Teo <teohuiming@gmail.com>
2017-06-27 17:54:18 +0800

    aliases: add `be`, remove unused alias

--- .zsh_custom/aliases.zsh
+++ .zsh_custom/aliases.zsh
@@ -2,5 +2,5 @@ alias g=git
 alias els=elasticsearch
 alias redis="redis-server /usr/local/etc/redis.conf"
 alias pg="postgres -D /usr/local/var/postgres"
-alias streamflix="~/streamflix/bin/streamflix"
+alias be="bundle exec"
 alias dots='git --git-dir=$HOME/.dots/ --work-tree=$HOME'
