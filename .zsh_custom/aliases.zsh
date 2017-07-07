13ca482fa139111a7ff1a08dacfa0e9fa540e0ce
Huiming Teo <teohuiming@gmail.com>
2017-07-07 20:49:25 +0800

    aliase: chrome

--- .zsh_custom/aliases.zsh
+++ .zsh_custom/aliases.zsh
@@ -4,3 +4,4 @@ alias redis="redis-server /usr/local/etc/redis.conf"
 alias pg="postgres -D /usr/local/var/postgres"
 alias be="bundle exec"
 alias dots='git --git-dir=$HOME/.dots/ --work-tree=$HOME'
+alias chrome="/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"
