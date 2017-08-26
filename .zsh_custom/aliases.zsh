af73e3b68b42a8c4ebfc41ff92047ffaa62c9105
Huiming Teo <teohuiming@gmail.com>
2017-08-26 18:22:07 +0800

    update brewfile, aliases

--- .zsh_custom/aliases.zsh
+++ .zsh_custom/aliases.zsh
@@ -5,3 +5,5 @@ alias pg="postgres -D /usr/local/var/postgres"
 alias be="bundle exec"
 alias dots='git --git-dir=$HOME/.dots/ --work-tree=$HOME'
 alias chrome="/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"
+alias bb="brew bundle --global"
+alias killfswatch="killall fsevent_watch; kill -9 \$(ps -A -ostat,ppid | awk '/[zZ]/{print \$2}')"
